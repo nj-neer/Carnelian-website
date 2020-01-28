d7af528997095710a27bbd25d7977192a69fdbda
Cyriaque <cyriaquedelaunay@gmail.com>
2020-01-28 21:11:00 +0100

    Added modal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,47 +1,119 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
-      <img
-        src="/assets/img/logo-hd.png"
-        width="24"
-        height="24"
-        class="d-inline-block"
-        alt="Carnelian icon"
-      />
-      Carnelian
-    </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/assets/img/logo-hd.png"
+          width="24"
+          height="24"
+          class="d-inline-block"
+          alt="Carnelian icon"
+        />
+        Carnelian
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
 
-        <li class="nav-item" data-toggle="tooltip" data-placement="bottom" title="Available soon !">
-          <a class="nav-link disabled" href="#">Download</a>
-        </li>
+          <li
+            class="nav-item"
+            data-toggle="tooltip"
+            data-placement="bottom"
+            title="Available soon !"
+          >
+            <a class="nav-link disabled" href="#">Download</a>
+          </li>
 
-        <li class="nav-item" data-toggle="tooltip" data-placement="bottom" title="Available soon !">
-          <a class="nav-link disabled" href="#">Docs</a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+          <li
+            class="nav-item"
+            data-toggle="tooltip"
+            data-placement="bottom"
+            title="Available soon !"
+          >
+            <a class="nav-link disabled" href="#">Docs</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item" data-toggle="modal" data-target="#exampleModal">
+            <a class="nav-link" href="#">Sign in</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="modal" tabindex="-1" id="exampleModal" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Log in to your account</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">
+                    <i class="mdi mdi-email"></i>
+                  </div>
+                </div>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">
+                    <i class="mdi mdi-key"></i>
+                  </div>
+                </div>
+
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+              </div>
+              <br />
+              <button type="submit" class="btn btn-primary btn-block">Submit</button>
+            </form>
+
+            <hr />
+            <center>
+              <span>OR</span>
+            </center>
+
+            <button type="submit" class="btn btn-outline-dark btn-block">
+              <i class="mdi mdi-github-circle"></i>
+              LOG IN WITH GITHUB
+            </button>
+
+            <button type="submit" class="btn btn-outline-danger btn-block">
+              <i class="mdi mdi-google"></i> LOG IN WITH GOOGLE
+            </button>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+          </div>
+        </div>
+      </div>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script lang="ts">
