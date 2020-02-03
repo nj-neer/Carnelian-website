a0aa799a7282f8b88972d187a10d4a1d51803a4a
Cyriaque <cyriaquedelaunay@gmail.com>
2020-02-03 21:22:12 +0100

    fixed issue with helper

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,15 +1,15 @@
-import md5 from "md5";
+import * as md5 from "md5";
 import IUser from "./interfaces/User.interface";
 
 /**
  * Generate a gravatar url from user
- * @param user 
+ * @param user
  */
 export function getGravatarUrl(user: IUser): string {
     if (!user || !user.email) {
         return "";
     }
-    const mailHash = md5(this.user.email);
+    const mailHash = md5(user.email);
     return "https://www.gravatar.com/avatar/" + mailHash;
 }
 
