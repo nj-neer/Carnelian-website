7c26ea2da8e772ffaa55acf04d0b87952878116d
Cyriaque <cyriaquedelaunay@gmail.com>
2020-02-09 19:41:29 +0100

    Updated express config

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 
 const app = express();
 app.use(express.static("dist"));
+app.use((req, res) => res.sendFile(`dist/index.html`));
 app.listen(process.env.PORT || 8080, () => {
     // tslint:disable-next-line: no-console
     console.log("Carnelian website listening on port " + (process.env.PORT || 8080));
